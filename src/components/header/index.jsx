49e99e0a35dd7c1e55ec2069b1ce9953d1bb9104
Matheus Henrique <matheushds.br@gmail.com>
2025-06-20 06:36:09 -0300

    feat: create userReducer and login and logout feature

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,19 +5,36 @@ import Cart from "../cart/index";
 
 // Styles
 import * as Styles from "./styles";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../redux/user/slice";
 
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userReducer.user);
 
   const handleCartClick = () => {
     setCartIsVisible(true);
   };
 
+  const handleLogin = () => {
+    dispatch(login({
+      name: "Matheus",
+      email: "matheushds.br@gmail.com"
+    }));
+  };
+
+  const handleLogout = () => {
+    dispatch(login());
+  };
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {user ? (
+          <div onClick={handleLogout}>{user.name} - Logout</div>) : (
+          <div onClick={handleLogin}>Login</div>)
+        }
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
