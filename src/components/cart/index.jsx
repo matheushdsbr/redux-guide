6da5833bb6f59afc0f4e6a23968b719d116d56bb
Matheus Henrique <matheushds.br@gmail.com>
2025-06-27 01:51:55 -0300

    refactor(components): adjust cart component to use the modal component

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,20 +1,21 @@
 // Styles
 import { useSelector } from "react-redux";
-import * as Styles from "./styles";
 import CartItem from "../cart-item";
+import Modal from "../modal";
 
-const Cart = ({ isVisible, setIsVisible }) => {
-  const handleEscapeAreaClick = () => setIsVisible(false);
+const Cart = () => {
   const products = useSelector((state) => state.cartReducer.products);
 
   return (
-    <Styles.CartContainer isVisible={isVisible}>
-      <Styles.CartEscapeArea onClick={handleEscapeAreaClick} />
-      <Styles.CartContent>
-        <Styles.CartTitle>Seu Carrinho</Styles.CartTitle>
-        {products.map(product => <CartItem key={product.id} product={product} />)}
-      </Styles.CartContent>
-    </Styles.CartContainer>
+    <Modal modalType="cart" title="Seu carrinho">
+      {products.length > 0 ? (
+        products.map((product) => (
+          <CartItem key={product.id} product={product} />
+        ))
+      ) : (
+        <p>Você ainda não tem produtos no carrinho.</p>
+      )}
+    </Modal>
   );
 };
 
