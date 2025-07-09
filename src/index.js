418b79d534d0ed11d8393f6c3dc38a09bf460917
Matheus Silva <matheushds.br@gmail.com>
2025-07-09 18:21:14 -0300

    feat: add graphql and apollo/client

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from './redux/store';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </Provider>
   </React.StrictMode>
 );
